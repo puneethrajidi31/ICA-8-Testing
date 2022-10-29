06d35c35aa47ee4d2719e4a917fc5c9bcfd35531
Puneeth Reddy Rajidi (Student) <prajidi@sundevils.asu.edu>
2022-10-28 19:14:18 -0700

    Final test cases with file exceptions handler

--- ICA8/src/urinals.java
+++ ICA8/src/urinals.java
@@ -1,18 +1,28 @@
 
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.IOException;
+import java.io.*;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.Scanner;
+import java.util.Vector;
 
-    public class urinals {
+public class urinals {
         public static void main(String[] args) throws IOException {
             //Author: Puneeth Reddy Rajidi
 
             //File reading
-            readfile("src\\file1.dat");
-
+            System.out.println("Enter 1 for reading the file and 2 for reading the string");
+            Scanner sc=new Scanner(System.in);
+            int x=sc.nextInt();
+            if(x==1) {
+                readfile("src\\urinals.dat");
+            }
+            else if(x==2) {
+                String t = stringfetch();
+                System.out.println("Possible number of Urinals are "+urinalscount(t));
+            }
+            else {
+                System.out.println("Invalid Input");
+            }
 
         }
 
@@ -125,19 +135,51 @@ import java.util.Scanner;
             }
             File file = new File(path);
             Scanner s = new Scanner(file);
-            Path output=Path.of("src\\output.dat");
+            Vector<Integer> v=new Vector<Integer>();
             while(s.hasNextLine()) {
                     String y=s.nextLine();
                     boolean gh = goodString(y);
                     if (gh == false) {
-                        Files.writeString(output,"-1");
+                        v.add(-1);
+                        break;
+                        //Stopping the loop as we got -1
 
                     }
                     else {
                         int x = urinalscount(y);
-                        Files.writeString(output,String.valueOf(x));
+                        v.add(x);
                     }
                 }
+            writefile(v);
+            }
+
+        public static String stringfetch(){
+            Scanner sc=new Scanner(System.in);
+            System.out.println("Enter the string:");
+            String str=sc.next();
+            return str;
+        }
+
+        public static void writefile(Vector<Integer>v) throws IOException {
+
+            File f=new File("src\\rule.dat");
+            File x=f;
+            int count=1;
+            //Checking if the file is duplicated or not and ensuring the proper file name
+            while(x.exists())
+            {
+                x=new File("src\\rule"+count+".dat");
+                count++;
+            }
+            f=x;
+
+                FileWriter p=new FileWriter(f.getAbsoluteFile());
+                BufferedWriter b=new BufferedWriter(p);
+                for(Integer i:v)
+                {
+                    b.write(i+"\n");
+                }
+                b.close();
             }
 
 
